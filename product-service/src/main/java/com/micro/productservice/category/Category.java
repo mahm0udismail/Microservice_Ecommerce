a9443c9f9a97c411c79a96f8db70dc8e95ac3832
Mahmoud Ismail <mahmoudismail9114@gmail.com>
2025-06-22 03:40:24 +0300

    Add Category entity and TestController for product service

---
+++ product-service/src/main/java/com/micro/productservice/category/Category.java
@@ -0,0 +1,28 @@
+package com.micro.productservice.category;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@Entity
+@Table(name = "Category")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false, unique = true)
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Onetomany(mappedBy = "category", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Product> products;
+}
