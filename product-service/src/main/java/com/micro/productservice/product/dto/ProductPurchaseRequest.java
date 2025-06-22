baf4ee3113947a75aaae93a2fb3e531f9eee900f
Mahmoud Ismail <mahmoudismail9114@gmail.com>
2025-06-23 02:18:25 +0300

    Refactor ProductPurchaseRequest to use record syntax and remove unnecessary annotations

--- product-service/src/main/java/com/micro/productservice/product/dto/ProductPurchaseRequest.java
+++ product-service/src/main/java/com/micro/productservice/product/dto/ProductPurchaseRequest.java
@@ -1,17 +1,13 @@
 package com.micro.productservice.product.dto;
-import org.springframework.data.mongodb.core.aggregation.ArrayOperators.In;
 
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 
-public class ProductPurchaseRequest {
-
+public record ProductPurchaseRequest(
     @NotNull(message = "Product ID cannot be null")
-    @NotBlank(message = "Product ID cannot be blank")
-    Integer productId;
+    Integer productId,
 
     @NotNull(message = "Quantity cannot be null")
-    @NotBlank(message = "Quantity cannot be blank")
-    double quantity;
-
-}
+    @Min(value = 1, message = "Quantity must be at least 1")
+    double quantity
+) {}
